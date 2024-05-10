227a345215ad6f6cc3ff81d9c58d42d8b7c40c7a
Micah Elliott <mde@micahelliott.com>
2024-05-10 15:08:33 -0700

    :sparkles: shorten to d, docker-compose, better podman
    And some more tips, fixes, shortenings
    
    Make sure you have free space for aliases: d, dk, dr
    
    I now use:
      alias ds=dirs
    
    This masks popular but infrequently used commands: dc, dd, df, du

--- init.sh
+++ init.sh
@@ -16,8 +16,9 @@
 # - full help for each mgmt cmd
 
 # Set a short-term default prefix; unset later
-p=${SCAD_PREFIX-dd}
-d=${SCAD_DOCKER-docker} # or maybe podman
+p=${SCAD_PREFIX-d}
+d=${SCAD_DOCKER-podman} # or maybe docker
+dc=${SCAD_DCOMPOSE-podman-compose} # or maybe $dc
 
 GRC_INSTALLED=false
 GRC=''
@@ -46,8 +47,9 @@ _grc() {
 
 ## Docker top-level command aliases
 dkr="${DOCKER-docker}"
-alias dr='docker'
+alias dr=$d
 alias ${p}'?'="_${p}HELP"
+alias ${p}="_${p}HELP"
 # alias ${p}'?'=ddH
 alias ${p}H='docker --help'
 # alias ${p}'?'='docker --help'
@@ -67,12 +69,12 @@ __SCAD_BUILDER_HELP="
 TODO
 "
 alias ${p}b'?'='echo $__SCAD_BUILDER_HELP'
-alias ${p}bH='docker builder'
-alias ${p}b='docker builder'
-alias ${p}bH='docker builder   build --help'
-alias ${p}bb='_grc docker builder build'
-alias ${p}bB='_grc docker builder build --tag'
-alias ${p}bpr='docker builder prune'
+alias ${p}bH="${d} builder"
+alias ${p}b="${d} builder"
+alias ${p}bH="${d} builder   build --help"
+alias ${p}bb="_grc ${d} builder build"
+alias ${p}bB="_grc ${d} builder build --tag"
+alias ${p}bpr="${d} builder prune"
 
 ## conFig (f)
 __SCAD_CONFIG_HELP="
@@ -82,13 +84,13 @@ alias ${p}f'?'='echo $__SCAD_CONFIG_HELP'
 alias ${p}f="$d config"
 alias ${p}fH="$d config"
 alias ${p}fcr="$d config create"
-alias ${p}fin="$d config inspect"
-alias ${p}fls="$d config ls"
+alias ${p}fi="$d config inspect"
+alias ${p}fl="$d config ls"
 alias ${p}frm="$d config rm"
 
 ## Container (c)
-__SCAD_CONTAINER_HELP="Docker CONTAINER commands:
-  ${p}c    — Invoke the bare docker container command
+__SCAD_CONTAINER_HELP="${d} CONTAINER commands:
+  ${p}c    — Invoke the bare ${d} container command
   ${p}ca   — Attach local standard input, output, and error streams to a running container
   ${p}cci  — Commit a new image from a container’s changes
   ${p}ccp  — Copy files/folders between a container and the local filesystem
@@ -101,8 +103,8 @@ __SCAD_CONTAINER_HELP="Docker CONTAINER commands:
   ${p}ck   — Kill one or more running containers
   ${p}cl   — Logs of a container
   ${p}cL   — Logs, in follow mode
-  ${p}cls  — List containers
-  ${p}clsa — List ALL containers, even those not running
+  ${p}cl   — List containers
+  ${p}cla  — List ALL containers, even those not running
   ${p}cp   — Pause all processes within one or more containers
   ${p}cpt  — Port mappings list or a specific mapping for the container
   ${p}cpr  — Prune/remove all stopped containers
@@ -122,8 +124,8 @@ __SCAD_CONTAINER_HELP="Docker CONTAINER commands:
   ${p}cw   — Wait/block until one or more containers stop, then print their exit codes
 "
 alias ${p}c'?'='echo $__SCAD_CONTAINER_HELP'
-alias ${p}cH="docker container"
-# alias ${p}C='docker container'
+alias ${p}cH="${d} container"
+# alias ${p}C='${d} container'
 alias ${p}c="$d container"
 alias ${p}ca="$d container attach"
 alias ${p}cci="$d container commit"
@@ -135,10 +137,10 @@ alias ${p}cei="$d container exec -i -t"
 alias ${p}cex="$d container export"
 alias ${p}cin="$d container inspect"
 alias ${p}ck="$d container kill"
-alias ${p}cl="$d container logs"
+alias ${p}cL="$d container logs"
 alias ${p}clf="$d container logs --follow"
-alias ${p}cls="$d container ls"
-alias ${p}clsa="$d container ls -a"
+alias ${p}cl="$d container ls"
+alias ${p}cla="$d container ls -a"
 alias ${p}cp="$d container pause"
 alias ${p}cpt="$d container port"
 alias ${p}cpr="$d container prune"
@@ -167,22 +169,22 @@ alias ${p}xH="$d context"
 alias ${p}xcr="$d context create"
 alias ${p}xex="$d context export"
 alias ${p}xim="$d context import"
-alias ${p}xin="$d context inspect"
-alias ${p}xls="$d context ls"
+alias ${p}xi="$d context inspect"
+alias ${p}xl="$d context ls"
 alias ${p}xrm="$d context rm"
 alias ${p}xup="$d context update"
 alias ${p}xu="$d context use"
 
 ## Image (i)
-__SCAD_IMAGE_HELP="Docker IMAGE commands:
-  ${p}i    — Invoke the bare docker image command
+__SCAD_IMAGE_HELP="${d} IMAGE commands:
+  ${p}i    — Invoke the bare ${d} image command
   ${p}ib   — Build an image from a Dockerfile
   ${p}iB   — Build an image with a tag
   ${p}ih   — Show the history of an image
   ${p}iim  — Import the contents from a tarball to create a filesystem image
-  ${p}iin  — Display detailed information on one or more images
+  ${p}ii   — Display detailed information on one or more images
+  ${p}il   — List images
   ${p}ild  — Load an image from a tar archive or STDIN
-  ${p}ils  — List images
   ${p}ipr  — Remove unused images
   ${p}ipl  — Pull an image or a repository from a registry
   ${p}iph  — Push an image or a repository to a registry
@@ -198,9 +200,9 @@ alias ${p}ib="$d image build"
 alias ${p}iB="$d image build -t"
 alias ${p}ih="$d image history"
 alias ${p}iim="$d image import"
-alias ${p}iin="$d image inspect"
+alias ${p}ii="$d image inspect"
+alias ${p}il="_grc $d image ls"
 alias ${p}ild="$d image load"
-alias ${p}ils="_grc $d image ls"
 alias ${p}ipr="$d image prune"
 alias ${p}ipl="_grc $d image pull"
 alias ${p}ipu="$d image push"
@@ -212,13 +214,13 @@ alias ${p}is="_grc $d search"
 
 ## Network (n)
 __SCAD_NETWORK_HELP="
-  ${p}nc  — Create a network
-  ${p}ncr — Connect a container to a network
-  ${p}nx  — Disconnect a container from a network
-  ${p}nin — Display detailed information on one or more networks
-  ${p}nls — List networks
-  ${p}npr — Prune, remove all unused networks
-  ${p}nrm — Remove one or more networks
+  ${p}nc   — Connect a container to a network
+  ${p}ncr  — Create a network
+  ${p}nx   — Disconnect a container from a network
+  ${p}ni   — Display detailed information on one or more networks
+  ${p}nl   — List networks
+  ${p}npr  — Prune, remove all unused networks
+  ${p}nrm  — Remove one or more networks
 "
 alias ${p}n'?'='echo $__SCAD_NETWORK_HELP'
 alias ${p}nH="$d network"
@@ -226,8 +228,8 @@ alias ${p}n="$d network"
 alias ${p}nc="$d network connect"
 alias ${p}ncr="$d network create"
 alias ${p}nx="$d network disconnect"
-alias ${p}nin="$d network inspect"
-alias ${p}nls="_grc $d network ls"
+alias ${p}ni="$d network inspect"
+alias ${p}nl="_grc $d network ls"
 alias ${p}npr="$d network prune"
 alias ${p}nrm="$d network rm"
 
@@ -241,9 +243,9 @@ alias ${p}p="$d plugin"
 alias ${p}pcr="$d plugin create"
 alias ${p}pdi="$d plugin disable"
 alias ${p}pen="$d plugin enable"
-alias ${p}pin="$d plugin inspect"
-alias ${p}pi="$d plugin install"
-alias ${p}pls="$d plugin ls"
+alias ${p}pi="$d plugin inspect"
+alias ${p}pI="$d plugin install"
+alias ${p}pl="$d plugin ls"
 alias ${p}ppu="$d plugin push"
 alias ${p}prm="$d plugin rm"
 alias ${p}ps="$d plugin set"
@@ -257,8 +259,8 @@ alias ${p}z'?'='echo $__SCAD_SECRET_HELP'
 alias ${p}zH="$d secret"
 alias ${p}z="$d secret"
 alias ${p}zcr="$d secret create"
-alias ${p}zin="$d secret inspect"
-alias ${p}zls="$d secret ls"
+alias ${p}zi="$d secret inspect"
+alias ${p}zl="$d secret ls"
 alias ${p}zrm="$d secret rm"
 
 ## Trust (t)
@@ -272,7 +274,7 @@ alias ${p}tkgen="$d trust key generate"
 alias ${p}tkld="$d trust key load"
 alias ${p}tsadd="$d trust signer add"
 alias ${p}tsrm="$d trust signer remove"
-alias ${p}tin="$d trust inspect"
+alias ${p}ti="$d trust inspect"
 alias ${p}trm="$d trust revoke"
 alias ${p}ts="$d trust sign"
 
@@ -284,9 +286,9 @@ TODO
 alias ${p}v'?'='echo $__SCAD_VOLUME_HELP'
 alias ${p}vH="$d volume"
 alias ${p}v="$d volume"
-alias ${p}vc="$d volume create"
-alias ${p}vin="$d volume inspect"
-alias ${p}vls="$d volume ls"
+alias ${p}vcr="$d volume create"
+alias ${p}vi="$d volume inspect"
+alias ${p}vl="$d volume ls"
 alias ${p}vpr="$d volume prune"
 alias ${p}vrm="$d volume rm"
 
@@ -309,7 +311,7 @@ alias ${p}k'?'='echo $__SCAD_STACK_HELP'
 alias ${p}kH="$d stack"
 alias ${p}k="$d stack"
 alias ${p}kd="$d stack deploy"
-alias ${p}kls="$d stack ls"
+alias ${p}kl="$d stack ls"
 alias ${p}kps="$d stack ps"
 alias ${p}krm="$d stack rm"
 alias ${p}ks="$d stack services"
@@ -320,36 +322,35 @@ __SCAD_COMPOSE_HELP="
 TODO
 "
 alias ${p}k'?'='echo $__SCAD_COMPOSE_HELP'
-alias ${p}kH='docker-compose'
-alias ${p}k='docker-compose'
-alias ${p}k='docker-compose'
-alias ${p}kb='docker-compose build'
-alias ${p}kB='docker-compose build --no-cache'
-alias ${p}kd='docker-compose down'
-alias ${p}ke='docker-compose exec'
-alias ${p}kk='docker-compose kill'
-alias ${p}kl='docker-compose logs'
-alias ${p}kL='docker-compose logs --follow'
-alias ${p}klf='docker-compose logs --follow'
-alias ${p}kls='_grc docker-compose ps'
-alias ${p}kps='_grc docker-compose ps'
-alias ${p}kp='docker-compose pause'
-alias ${p}kP='docker-compose unpause'
-alias ${p}kpl='docker-compose pull'
-alias ${p}kph='docker-compose push'
-alias ${p}kps='docker-compose ps'
-alias ${p}kr='docker-compose run'
-alias ${p}kR='docker-compose run --rm'
-alias ${p}krm='docker-compose rm'
-alias ${p}ks='docker-compose start'
-alias ${p}ksc='docker-compose scale'
-alias ${p}kcy='docker-compose restart'
-alias ${p}ku='docker-compose up'
-alias ${p}kud='docker-compose up -d'
-alias ${p}kv='docker-compose version'
-alias ${p}kx='docker-compose stop'
+alias ${p}kH="$dc"
+alias ${p}k="$dc"
+alias ${p}k="$dc"
+alias ${p}kb="$dc build"
+alias ${p}kB="$dc build --no-cache"
+alias ${p}kd="$dc down"
+alias ${p}ke="$dc exec"
+alias ${p}kk="$dc kill"
+alias ${p}kL="$dc logs"
+alias ${p}kLf="$dc logs --follow"
+alias ${p}kl="_grc $dc ls"
+alias ${p}kps="_grc $dc ps"
+alias ${p}kp="$dc pause"
+alias ${p}kP="$dc unpause"
+alias ${p}kpl="$dc pull"
+alias ${p}kph="$dc push"
+alias ${p}kps="$dc ps"
+alias ${p}kr="$dc run"
+alias ${p}kR="$dc run --rm"
+alias ${p}krm="$dc rm"
+alias ${p}ks="$dc start"
+alias ${p}ksc="$dc scale"
+alias ${p}kcy="$dc restart"
+alias ${p}ku="$dc up"
+alias ${p}kud="$dc up -d"
+alias ${p}kv="$dc version"
+alias ${p}kx="$dc stop"
 
-#${p}kudf () { docker-compose -f $1 up -d }
+#${p}kudf () { $dc -f $1 up -d }
 
 
 ## CleanUp (rm)
@@ -416,15 +417,20 @@ SYSTEMS
 HELP
   ${p}a apropos (search for any keyword in command descriptions)
   ${p}? help (this)
+  ${p}  help (this)
   --help (put on the end of any alias)
-  dr (docker itself, alone prints full help)
+  dr (docker/podman itself, alone prints full help)
 
 TIPS
   - Tab-complete after typing an alias
   - Invoke the base command for more flexible explicit option use
-  - Put command options at the end of any alias"
+  - Put additional command options at the end of any alias
+  - Add a '?' to the end of any command
+  - Use '^x a' to auto-expand the alias
+  - Put 'export SCAD_DOCKER=podman' in your shell startup file"
 
-_ddHELP () { echo $__SCAD_TOPLEVEL_HELP }
+
+_dHELP () { echo $__SCAD_TOPLEVEL_HELP }
 
 # Apropos
 __SCAD_HELPS=( $__SCAD_TOPLEVEL_HELP
@@ -438,6 +444,11 @@ __SCAD_HELPS=( $__SCAD_TOPLEVEL_HELP
                $__SCAD_VOLUME_HELP
                $__SCAD_IMAGE_HELP
                $__SCAD_COMPOSE_HELP )
-dda() { for h in $__SCAD_HELPS; do echo $h; done | grep -E '^  dd\w+ +— ' | grep -i --color=always $1 }
+da() {
+    if (( $#argv == 1 ))
+    then for h in $__SCAD_HELPS; do echo $h; done | grep -E '^  d\w+ +— ' | grep -i --color=always ${1}
+    else for h in $__SCAD_HELPS; do echo $h; done | grep -E --color=none '^  d\w+ +— '
+    fi
+}
 
-unset p d
+unset p d dc
